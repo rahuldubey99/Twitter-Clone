dcac8b9e441f32b47b042278f5298ff384aa4957
ahuldubeycse <rahulsharma9451660093@gmail.com>
2020-09-02 16:09:36 +0530

    first commit

---
+++ src/component/Twittes.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Twittes = () => {
+    return (
+        <div className="posts">
+        <div className="posts_home">Home</div>
+
+            
+        </div>
+    )
+}
+
+export default Twittes
