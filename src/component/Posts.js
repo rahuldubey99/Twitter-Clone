04bd6b616a7d956aceab88952d5b88de12d57e8c
ahuldubeycse <rahulsharma9451660093@gmail.com>
2020-09-07 20:01:31 +0530

    Second

--- src/component/Posts.js
+++ src/component/Posts.js
@@ -1,9 +1,18 @@
 import React from 'react'
-
+import {FaRegCheckCircle} from "react-icons/fa"
 const Posts = () => {
     return (
-        <div className="posts">
-            <div className="posts_first"></div>            
+        <div className="postss">
+            <div className="postss_first">
+                <div className="posts_first_img">
+                    <img src="/images/1.jpeg" alt="profile img"/>
+                </div>
+                <div className="posts_first_name"><strong>Rahul dubey</strong><FaRegCheckCircle className="verify"/></div>
+                <div className="posts_first_username">@info<span>6m</span></div>
+            </div>            
+            <div className="postss_details">
+                
+            </div>
         </div>
     )
 }
--- src/component/Twittes.js
+++ src/component/Twittes.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import CreateTweet from './CreateTweet'
+import Posts from './Posts'
 
 const Twittes = () => {
     return (
         <div className="posts">
         <div className="posts_home">Home</div>
         <CreateTweet/>
+        <Posts/>
             
         </div>
     )
